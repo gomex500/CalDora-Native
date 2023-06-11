b9eb0734e96c79d3aa26d8365c2083665903da22
gomex500 <gomezfreddy886@gmail.com>
2023-06-10 22:42:32 -0600

    se creo las vistas principales

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -29,6 +29,130 @@ const Calculadora = () =>{
     const limpiarCampos = () =>{
         setResultado("");
     };
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.resultContainer}>
+                <Text style={styles.resultText}>{resultado}</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('7')}
+                    >
+                        <Text style={styles.buttonText}> 7 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('8')}
+                    >
+                        <Text style={styles.buttonText}> 8 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('9')}
+                    >
+                        <Text style={styles.buttonText}> 9 </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('4')}
+                    >
+                        <Text style={styles.buttonText}> 4 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('5')}
+                    >
+                        <Text style={styles.buttonText}> 5 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('6')}
+                    >
+                        <Text style={styles.buttonText}> 6 </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('1')}
+                    >
+                        <Text style={styles.buttonText}> 1 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('2')}
+                    >
+                        <Text style={styles.buttonText}> 2 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('3')}
+                    >
+                        <Text style={styles.buttonText}> 3 </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('0')}
+                    >
+                        <Text style={styles.buttonText}> 0 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('.')}
+                    >
+                        <Text style={styles.buttonText}> . </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={CalcularResultado}
+                    >
+                        <Text style={styles.buttonText}> = </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('+')}
+                    >
+                        <Text style={styles.buttonText}> + </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('-')}
+                    >
+                        <Text style={styles.buttonText}> - </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('*')}
+                    >
+                        <Text style={styles.buttonText}> * </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.row}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => BtnPress('/')}
+                    >
+                        <Text style={styles.buttonText}> / </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={limpiarCampos}
+                    >
+                        <Text style={styles.buttonText}> C </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 export default Calculadora;
