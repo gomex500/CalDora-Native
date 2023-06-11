549902621b2201cc821b017be1b8cf804fe46e72
gomex500 <gomezfreddy886@gmail.com>
2023-06-10 22:24:28 -0600

    se creo el estado resultado inicializando en vacio

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,11 +1,14 @@
+// importaciones generares
 import React, {useState} from "react";
 import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
 import {create, all} from "mathjs";
 
+// inicializando math
 const math = create(all);
 
 const Calculadora = () =>{
-
+    // estado de resultado
+    const [resultado, setResultado] = useState('');
 }
 
 export default Calculadora;
