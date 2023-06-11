de259bc48695f4e94ff6f93e855ffac96699c840
gomex500 <gomezfreddy886@gmail.com>
2023-06-10 22:49:53 -0600

    se creo los estilos principales

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -155,4 +155,40 @@ const Calculadora = () =>{
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    resultContainer: {
+        marginBottom: 20
+    },
+    resultText: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    buttonContainer: {
+        width: 300
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
+        margin: 4
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
+
 export default Calculadora;
